5b2bfa47251978f23b09a1cf5fea5ef01711505b
Angela Gao <Xuan4781@users.noreply.github.com>
2025-10-24 11:53:40 -0400

    Changed to a different template, changed undefined to N/A and added new column values to populate

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import fs from 'fs';
 import xlsx from 'xlsx';
 import PizZip from 'pizzip';
 import Docxtemplater from 'docxtemplater';
+import { execFile } from 'child_process';
 
 // Setup
 const app = express();
@@ -19,7 +20,7 @@ let buildingData = []; // excel data
 // loads data from acel into buildingdata
 function loadExcelData() {
     try {
-        // const workbook = xlsx.readFile('master_sheet.xlsx');
+        // const workbook = xlsx.readFile('final_merged.xlsx');
         const workbook = xlsx.readFile('final_merged.xlsx');
         const sheetName = workbook.SheetNames[0];
         buildingData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName], {cellDates: true});
@@ -30,6 +31,49 @@ function loadExcelData() {
         // if file doesnt load, server doesnt run
         if (buildingData.length === 0) process.exit(1); 
         return false;
+    }
+}
+
+function mapExcel(excelData){
+    return {
+        'Address': excelData['Address'],
+        'Building_OwnerManager': excelData['Building Owner/Manager'],
+        'Use Type': excelData['Use Type'],
+        'Block': excelData['Block'],
+        'BIN': excelData['BIN'],
+        'Borough': excelData['Borough'],
+        'Year Built': excelData['Year Built'],
+        'M Floors': excelData['M Floors'],
+        'Approx_Sq_Ft': excelData['Approx Sq Ft'],
+        'Landmark': excelData['Landmark'],
+        'Parking Garage (Yes/No)': excelData['Parking Garage'],
+        //'FISP Compliance Status': excelData['FISP Compliance Status'],
+        'Sub': excelData['Sub'],
+        'FISP Filing Due': excelData['FISP Filing Due'],
+        'FISP Last Filing Status': excelData['FISP Last Filing Status'],
+        'FISP Cycle Filing Window': excelData['FISP Cycle Filing Window'],
+        'LL126 Compliance Status': excelData['LL126 Compliance Status'],
+        'LL126 Cycle': excelData['LL126 Cycle'],
+        'LL126 Previous Filing Status': excelData['LL126 Previous Filing Status'],
+        'LL126 SREM Recommended Date': excelData['LL126 SREM Recommended Date'],
+        'LL126 Filing Window': excelData['LL126 Filing Window'],
+        'LL126 Next Steps': excelData['LL126 Next Steps'],
+        'LL126 Parapet Compliance Status': excelData['LL126 Parapet Compliance Status'],
+        'LL84 Compliance Status': excelData['LL84 Compliance Status'],
+        'LL84 Filing Due': excelData['LL84 Filing Due'],
+        'LL84 Next Steps': excelData['LL84 Next Steps'],
+        'LL87 Compliance Status': excelData['LL87 Compliance Status'],
+        'LL87 Filing Due': excelData['LL87 Filing Due'],
+        'LL87 Compliance Year': excelData['LL87 Compliance Year'],
+        'LL87 Next Steps': excelData['LL87 Next Steps'],
+        'LL88 Compliance Status': excelData['LL88 Compliance Status'],
+        'LL88 Filing Due': excelData['LL88 Filing Due'],
+        'LL88 Notes': excelData['LL88 Notes'],
+        'LL97 Compliance Status': excelData['LL97 Compliance Status'],
+        'LL97 Filing Due': excelData['LL97 Filing Due'],
+        'LL97 Next Steps': excelData['LL97 Next Steps'],
+        'Contact Email': excelData['Contact Email'],
+        'Contact Phone': excelData['Contact Phone']
     }
 }
 
@@ -65,7 +109,6 @@ app.post('/api/search-address', (req, res) => {
     }
 });
 
-// generating report 
 app.post('/api/generate-report', (req, res) => {
     const { address } = req.body;
     if (!address) {
@@ -80,9 +123,31 @@ app.post('/api/generate-report', (req, res) => {
     if (!reportData) {
         return res.status(404).json({ error: 'Data not found for report generation.' });
     }
+
+    
+    const mappedData = mapExcel(reportData);
+    
+    
+    const processedData = {};
+    for (const [key, value] of Object.entries(mappedData)) {
+        let processedValue = value;
+        const stringValue = String(value).trim().toLowerCase();
+        
+        if (value === undefined || 
+            value === null || 
+            value === '' ||
+            stringValue === '' ||
+            stringValue === 'undefined' ||
+            stringValue === 'null' ||
+            stringValue === 'nan' ||
+            Number.isNaN(value)) {
+            processedValue = 'N/A';
+        }    
+        processedData[key] = processedValue;
+    }
     
     try {
-        const templatePath = path.resolve("./temp.docx");
+        const templatePath = path.resolve("./Building Report Card_Word Template.docx");
         const content = fs.readFileSync(templatePath, "binary");
 
         const zip = new PizZip(content);
@@ -92,7 +157,7 @@ app.post('/api/generate-report', (req, res) => {
             delimiters: { start: '«', end: '»' }, 
         });
 
-        doc.render(reportData); 
+        doc.render(processedData);
 
         const buf = doc.getZip().generate({ type: "nodebuffer" });
 
